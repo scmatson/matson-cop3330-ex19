f2fee89565501d714e49d11820291e02283a8820
Scott <scmatson23@gmail.com>
2021-06-06 18:24:55 -0400

    Exercise 19 Solution

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -1,4 +1,9 @@
 package base;
+
+import java.text.DecimalFormat;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /*
  *  UCF COP3330 Summer 2021 Assignment 1 Solution
  *  Copyright 2021 Scott Matson
@@ -35,4 +40,47 @@ Challenges
 -Use a GUI interface with sliders for height and weight. Update the user interface on the fly. Use colors as well as text to indicate health.
  */
 public class App {
+    static Scanner ui = new Scanner(System.in);
+    static DecimalFormat df = new DecimalFormat("##0.0#");
+
+    public static void main(String[] args) {
+
+        System.out.print("Enter your weight in pounds: ");
+        double weight = isNumeric();                            //To consider scales with dec. places
+        System.out.print("Enter your height in inches: ");
+        double height = isNumeric();
+
+        double BMI = (weight / (height * height)) * 703;
+
+        System.out.println("Your BMI is " + df.format(BMI) + ".");
+
+        if(BMI > 18.5 && BMI < 25)
+        {
+            System.out.println("You are within the ideal weight range.");
+        }
+        else if(BMI > 25)
+        {
+            System.out.println("You are overweight. You should see your doctor.");
+        }
+        else
+        {
+            System.out.println("You are underweight. You should eat more or see your doctor.");
+        }
+    }
+
+    public static double isNumeric()
+    {
+        while(true)
+        {
+            try
+            {
+                return ui.nextDouble();
+            }
+            catch(InputMismatchException e)
+            {
+                ui.next();
+                System.out.print("Please enter a number: ");
+            }
+        }
+    }
 }
